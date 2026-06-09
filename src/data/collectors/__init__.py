bcb0abdb59c07522d20a162ff44675cc47cb750e
kakaCat <253834671@qq.com>
2026-06-09 10:31:20 +0800

    feat: add index data collector

--- src/data/collectors/__init__.py
+++ src/data/collectors/__init__.py
@@ -1,4 +1,5 @@
 # src/data/collectors/__init__.py
 from .stock_data import StockDataCollector
+from .index_data import IndexDataCollector
 
-__all__ = ['StockDataCollector']
+__all__ = ['StockDataCollector', 'IndexDataCollector']
